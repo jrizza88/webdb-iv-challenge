defed4d94d2ea056a02cd85763cd7c6c367d2ca7
jrizza88 <jetorres88@gmail.com>
2019-03-22 00:37:53 -0400

    updated getDish id, and insert methods

--- data/dbHelpers.js
+++ data/dbHelpers.js
@@ -5,8 +5,46 @@ const dbConfig = require('../knexfile');
 const db = knex(dbConfig.development);
 
 module.exports = {
+getDishes,
+getDish,
+addDish,
+getRecipes,
+addRecipe
+};
     
-    getDishes: () => {
+   function getDishes() {
         return db('dishes');
-    },
-}
+    }
+
+    function getDish(id){
+        // return db('dishes').where({id: Number(id)});
+        return db('dishes')
+        .where({'dishes.id': Number(id)})
+        .first()
+        .then(dish => {
+            return db
+            .select('recipe')
+            .from('recipes')
+            .where({'dish_id': dish.id})
+            // .joinRaw(dish)
+            .then(recipes => ({dish: dish, recipes:recipes}))
+            
+        })
+  
+    }
+
+   function addDish (dish) {
+        return db('dishes')
+        .insert(dish)
+        .then(ids => ({ id: ids[0]}))
+    }
+
+
+
+    function getRecipes() {
+        return db('recipes')
+    }
+
+    function addRecipe(recipe){
+        return db('recipes').insert(recipe).into('recipes')
+    }
--- index.js
+++ index.js
@@ -2,9 +2,39 @@
 
 const db = require('./data/dbHelpers');
 
-db.getDishes()
+// db.getDishes()
+// .then(dishes => {
+//     console.log('dishes:' , dishes)
+// }).catch(err =>{
+//     console.log('issues with getting dishes', err)
+// });
+
+
+db.getDish(2)
 .then(dishes => {
     console.log('dishes:' , dishes)
 }).catch(err =>{
     console.log('issues with getting dishes', err)
 });
+
+// db.getRecipes() 
+//     .then(recipes => {
+//         console.log('recipes with dishes', recipes)
+//     }).catch(err => {
+//         console.log('issues with getting recipes', err)
+//     });
+
+
+// db.addDish('Churros')
+// .then(function (dish){
+//     console.log('new dish: ', dish)
+//     res.json(dish)
+// }).catch(err =>{
+//     console.log('issues with getting dishes', err)
+// });
+
+// db.addDish('Ice Cream')
+// .then(response => {
+//     console.log(response)
+//    })
+//    .catch(err => {console.log(err)});
